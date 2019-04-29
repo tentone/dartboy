051dd649a6d2e7a5a428563c8000ce3e73070394
Tentone <tentone@outlook.com>
2019-04-29 09:35:45 +0100

    Windows / linux desktop project

--- dartboy/lib/main.dart
+++ dartboy/lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'gui/main_screen.dart';
+
 void main()
 {
   runApp(MyApp());
@@ -41,62 +43,6 @@ class MyApp extends StatelessWidget
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'GBC'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget
-{
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>
-{
-  int _counter = 0;
-
-
-  void _incrementCounter()
-  {
-    setState(()
-    {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-    (
-      appBar: AppBar
-      (
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column
-        (
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
