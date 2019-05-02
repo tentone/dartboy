1442303ead6dcd5a2bc4eb439b1c59fe2164419a
Tentone <tentone@outlook.com>
2019-05-02 19:55:46 +0100

    Derp

---
+++ dartboy/lib/emulator/emulator.dart
@@ -0,0 +1,8 @@
+
+/// Main emulator object used to directly interact with the system.
+///
+/// GUI communicates with this object, it is responsible for providing image, handling key input and user interaction.
+class Emulator
+{
+
+}
--- dartboy/lib/emulator/io.dart
+++ dartboy/lib/emulator/io.dart
@@ -1,8 +1,15 @@
+import 'memory.dart';
 
 /// Class to handle the IO function of the GBC
 ///
 /// Handlers gamepad input and serial communication.
 class IO
 {
+  /// System memory used to read and write IO data.
+  Memory memory;
 
+  IO(Memory memory)
+  {
+    this.memory = memory;
+  }
 }
