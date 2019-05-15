ccf39acf44bd2d1537cb167215c2cf8ef2ef0de1
tentone <tentone@outlook.com>
2019-05-15 22:54:21 +0100

    IO

---
+++ dartboy/lib/emulator/io.dart
@@ -0,0 +1,11 @@
+class IO
+{
+  static const int KEY_A = 0;
+  static const int KEY_B = 1;
+  static const int KEY_RIGHT = 2;
+  static const int KEY_LEFT = 3;
+  static const int KEY_UP = 4;
+  static const int KEY_DOWN = 5;
+  static const int KEY_START = 6;
+  static const int KEY_SELECT = 7;
+}
