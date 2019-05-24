2e32ba22739e773cd3b87460816e27379645df73
tentone <tentone@outlook.com>
2019-05-24 20:15:57 +0100

    Derp

--- dartboy/lib/emulator/mmu/mbc1.dart
+++ dartboy/lib/emulator/mmu/mbc1.dart
@@ -1,14 +1,25 @@
+import '../../emulator/cartridge/cartridge.dart';
+import '../../emulator/memory/memory.dart';
+
 import 'mmu.dart';
 
 class MBC1 implements MMU
 {
+  MBC1(Memory memory, Cartridge cartridge)
+  {
+    super(memory, cartridge);
+
+  }
+
   @override
-  void readByte(int address) {
+  int readByte(int address)
+  {
     // TODO: implement readByte
   }
 
   @override
-  void writeByte(int address, int value) {
+  void writeByte(int address, int value)
+  {
     // TODO: implement writeByte
   }
 
--- dartboy/lib/emulator/mmu/mmu.dart
+++ dartboy/lib/emulator/mmu/mmu.dart
@@ -1,7 +1,31 @@
 
-abstract class MMU
-{
-  void writeByte(int address, int value);
+import '../../emulator/cartridge/cartridge.dart';
+import '../../emulator/memory/memory.dart';
 
-  void readByte(int address);
+/// The MMU (memory management unit) is used to access memory.
+///
+/// Depending on the cartridge type it may select different data sources and switch memory banks as required.
+///
+/// This is the base implementation that considers only a sing ROM bank in the cartridge and a access to the system memory.
+class MMU
+{
+  Memory memory;
+
+  Cartridge cartridge;
+
+  MMU(Memory memory, Cartridge cartridge)
+  {
+    this.memory = memory;
+    this.cartridge = cartridge;
+  }
+
+  void writeByte(int address, int value)
+  {
+    this.memory.writeByte(address, value);
+  }
+
+  int readByte(int address)
+  {
+    this.memory.readByte(address);
+  }
 }
