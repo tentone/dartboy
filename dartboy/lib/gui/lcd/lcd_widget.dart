78e0468ba03f6d402f2c45e936962e6d04776be2
tentone <tentone@outlook.com>
2019-06-20 17:42:26 +0100

    Clean up

--- dartboy/lib/gui/lcd/lcd_widget.dart
+++ dartboy/lib/gui/lcd/lcd_widget.dart
@@ -74,30 +74,6 @@ class LCDPainter extends CustomPainter
     }
   }
 
-  int time = 0;
-
-  void drawTestImage(Canvas canvas, Size size)
-  {
-    time++;
-
-    for(int x = 0; x < LCD_WIDTH; x++)
-    {
-      for(int y = 0; y < LCD_HEIGHT; y++)
-      {
-        Paint color = new Paint();
-        color.style = PaintingStyle.stroke;
-        color.strokeWidth = 1.0;
-        color.color = Color.fromRGBO(((x + time).toDouble() / LCD_WIDTH.toDouble() * 255.0).toInt(), (y.toDouble() / LCD_HEIGHT.toDouble() * 255.0).toInt(), 0, 1.0);
-
-        List<double> points = new List<double>();
-        points.add(x.toDouble() - LCD_WIDTH / 2);
-        points.add(y.toDouble() + LCD_HEIGHT / 2);
-
-        canvas.drawRawPoints(PointMode.points, new Float32List.fromList(points), color);
-      }
-    }
-  }
-
   @override
   bool shouldRepaint(LCDPainter oldDelegate)
   {
