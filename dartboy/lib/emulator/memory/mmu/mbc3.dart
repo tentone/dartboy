e323b8783f64ace4347b02cf69584887334dde4f
tentone <tentone@outlook.com>
2019-06-26 09:18:41 +0100

    Derp

--- dartboy/lib/emulator/memory/mmu/mbc3.dart
+++ dartboy/lib/emulator/memory/mmu/mbc3.dart
@@ -21,34 +21,33 @@ class MBC3 extends MBC
     this.cartRam = new List<int>(MBC.RAM_PAGESIZE * 4);
   }
 
-  void writeByte(int address, int _data)
+  void writeByte(int address, int value)
   {
     address &= 0xffff;
-    int data = _data & 0xff;
+    value = value & 0xff;
 
-    switch (address & 0xF000)
+    switch(address & 0xF000)
     {
       case 0x0000:
       case 0x1000:
         if(this.cpu.cartridge.ramBanks != 0)
         {
-          this.ramEnabled = (data & 0x0F) == 0x0A;
+          this.ramEnabled = (value & 0x0F) == 0x0A;
         }
 
-        this.rtcEnabled = (data & 0x0F) == 0x0A;
+        this.rtcEnabled = (value & 0x0F) == 0x0A;
         break;
       case 0x2000:
       case 0x3000:
         // Same as for MBC1, except that the whole 7 bits of the RAM Bank Number are written directly to this address.
-        this.romPageStart = Memory.ROM_PAGESIZE * max(data & 0x7F, 1);
+        this.romPageStart = Memory.ROM_PAGESIZE * max(value & 0x7F, 1);
         break;
       case 0x4000:
       case 0x5000:
         // As for the MBC1s RAM Banking Mode, writing a value in range for 00h-03h maps the corresponding external RAM Bank (if any) into memory at A000-BFFF.
         // When writing a value of 08h-0Ch, this will map the corresponding RTC register into memory at A000-BFFF.
         // That register could then be read/written by accessing any address in that area, typically that is done by using address A000.
-
-        if ((data >= 0x08) && (data <= 0x0C))
+        if((value >= 0x08) && (value <= 0x0C))
         {
           // RTC
           if(this.rtcEnabled)
@@ -57,9 +56,9 @@ class MBC3 extends MBC
             this.ramBank = -1;
           }
         }
-        else if(data <= 0x03)
+        else if(value <= 0x03)
         {
-          this.ramBank = data;
+          this.ramBank = value;
           this.ramPageStart = this.ramBank * MBC.RAM_PAGESIZE;
         }
         break;
@@ -68,17 +67,16 @@ class MBC3 extends MBC
         //Depending on the current Bank Number/RTC Register selection (see above), this memory space is used to access an 8KByte external RAM Bank, or a single RTC Register.
         if(this.ramEnabled && this.ramBank >= 0)
         {
-          this.cartRam[address - 0xA000 + ramPageStart] = data;
+          this.cartRam[address - 0xA000 + ramPageStart] = value;
         }
         else if(this.rtcEnabled)
         {
           // TODO <RTC READ>
-          //  rtc[ramBank - 0x08] = data;
-          // System.err.println("Write to RTC not implemented yet");
+          this.rtc[ramBank - 0x08] = value;
         }
         break;
       default:
-        super.writeByte(address, _data);
+        super.writeByte(address, value);
         break;
     }
   }
