9854871a93b1a851c3e764a2da498da440eee9a1
tentone <tentone@outlook.com>
2019-07-24 10:24:06 +0100

    Clean up

--- dartboy/lib/emulator/memory/memory.dart
+++ dartboy/lib/emulator/memory/memory.dart
@@ -25,24 +25,21 @@ class Memory
   /// Size of a page of ROM, in bytes. 16kb.
   static const int ROM_PAGESIZE = 0x4000;
 
-  /// Register values, mapped from 0xFF00-0xFF7F + HRAM (0xFF80-0xFFFE) + Interrupt Enable Register (0xFFFF)
+  /// Register values contains mostly control flags, mapped from 0xFF00-0xFF7F + HRAM (0xFF80-0xFFFE) + Interrupt Enable Register (0xFFFF)
   List<int> registers;
-  //Uint8List registers;
 
   /// Sprite Attribute Table, mapped from 0xFE00-0xFE9F.
+  /// OAM (Object Attribute Memory)
   List<int> oam;
-  //Uint8List oam;
 
   /// Video RAM, mapped from 0x8000-0x9FFF.
   /// On the GBC, this bank is switchable 0-1 by writing to 0xFF4F.
   List<int> vram;
-  //Uint8List vram;
 
   /// Work RAM, mapped from 0xC000-0xCFFF and 0xD000-0xDFFF.
   ///
   /// On the GBC, this bank is switchable 1-7 by writing to 0xFF07.
   List<int> wram;
-  //Uint8List wram;
 
   /// The current page of Video RAM, always multiples of VRAM_PAGESIZE.
   ///
@@ -83,19 +80,15 @@ class Memory
     this.romPageStart = Memory.ROM_PAGESIZE;
 
     this.registers = new List<int>(0x100);
-    //this.registers = new Uint8List(0x100);
     this.registers.fillRange(0, this.registers.length, 0);
 
     this.oam = new List<int>(0xA0);
-    //this.oam = new Uint8List(0xA0);
     this.oam.fillRange(0, this.oam.length, 0);
 
     this.wram = new List<int>(Memory.WRAM_PAGESIZE * (this.cpu.cartridge.gameboyType == GameboyType.COLOR ? 8 : 2));
-    //this.wram = new Uint8List(Memory.WRAM_PAGESIZE * (this.cpu.cartridge.gameboyType == GameboyType.COLOR ? 8 : 2));
     this.wram.fillRange(0, this.wram.length, 0);
 
     this.vram = new List<int>(Memory.VRAM_PAGESIZE * (this.cpu.cartridge.gameboyType == GameboyType.COLOR ? 2 : 1));
-    //this.vram = new Uint8List(Memory.VRAM_PAGESIZE * (this.cpu.cartridge.gameboyType == GameboyType.COLOR ? 2 : 1));
     this.vram.fillRange(0, this.vram.length, 0);
 
     for(int i = 0; i < 0x100; i++)
