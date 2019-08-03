c225597539c8fa65484f5127e52f5db9122f2e46
tentone <tentone@outlook.com>
2019-08-03 17:15:51 +0100

    Clean up

--- dartboy/lib/emulator/graphics/bitmap.dart
+++ dartboy/lib/emulator/graphics/bitmap.dart
@@ -3,18 +3,21 @@ import 'dart:typed_data';
 /// Stores bitmap data, only the actual image data.
 class Bitmap
 {
-  final int pixelLength;
+  final int pixelSize;
   final int width;
   final int height;
-  final Uint8List contentByteData;
+  final Uint8List data;
 
-  Bitmap(this.width, this.height, this.contentByteData, {this.pixelLength = 4});
+  Bitmap(this.width, this.height, this.data, {this.pixelSize = 4});
 
-  int get size{return (this.width * this.height) * this.pixelLength;}
+  int size()
+  {
+    return (this.width * this.height) * this.pixelSize;
+  }
 
   Bitmap copy()
   {
-    return Bitmap(this.width, this.height, Uint8List.fromList(this.contentByteData), pixelLength: this.pixelLength);
+    return Bitmap(this.width, this.height, Uint8List.fromList(this.data), pixelSize: this.pixelSize);
   }
 }
 
@@ -26,37 +29,38 @@ class BitmapFile
   Uint8List header;
   Bitmap image;
 
-  set contentByteData(Uint8List contentByteData)
+  /// Get the full length of the bitmap file (header and data).
+  ///
+  int length()
   {
-    this.image = new Bitmap(this.content.width, this.content.height, contentByteData, pixelLength: this.content.pixelLength);
+    return BitmapFile.headerSize + this.image.size();
   }
 
-  Bitmap get content{return this.content;}
-  Uint8List get headerByteData{return this.header;}
-  int get length{return BitmapFile.headerSize + this.image.size;}
-
-  Uint8List get data
+  /// Get the bitmap file byte data
+  Uint8List getFile()
   {
-    return Uint8List.fromList(this.header)..setRange(BitmapFile.headerSize, this.length, this.content.contentByteData);
+    Uint8List data = Uint8List.fromList(this.header);
+    data.setRange(BitmapFile.headerSize, this.length(), this.image.data);
+    return data;
   }
 
   BitmapFile(this.image)
   {
-    this.header = new Uint8List(length);
+    this.header = new Uint8List(this.length());
 
-    /// ARGB32 header
-    final ByteData bd = this.headerByteData.buffer.asByteData();
+    // ARGB32 header
+    final ByteData bd = this.header.buffer.asByteData();
     bd.setUint8(0x0, 0x42);
     bd.setUint8(0x1, 0x4d);
-    bd.setInt32(0x2, this.length, Endian.little);
+    bd.setInt32(0x2, this.length(), Endian.little);
     bd.setInt32(0xa, BitmapFile.headerSize, Endian.little);
     bd.setUint32(0xe, 108, Endian.little);
-    bd.setUint32(0x12, this.content.width, Endian.little);
-    bd.setUint32(0x16, -this.content.height, Endian.little);
+    bd.setUint32(0x12, this.image.width, Endian.little);
+    bd.setUint32(0x16, -this.image.height, Endian.little);
     bd.setUint16(0x1a, 1, Endian.little);
     bd.setUint32(0x1c, 32, Endian.little); // Pixel size
     bd.setUint32(0x1e, 3, Endian.little); // Bit fields
-    bd.setUint32(0x22, this.content.size, Endian.little);
+    bd.setUint32(0x22, this.image.size(), Endian.little);
     bd.setUint32(0x36, 0x000000ff, Endian.little);
     bd.setUint32(0x3a, 0x0000ff00, Endian.little);
     bd.setUint32(0x3e, 0x00ff0000, Endian.little);
